0790fa22bfa933b4060f41dcf3bf7788ec867ded
noemievincent <noemie.vincent1@student.hepl.be>
2022-01-08 14:42:30 +0100

    init

--- starter/DOM-04-p8-couleurs/couleurs.js
+++ starter/DOM-04-p8-couleurs/couleurs.js
@@ -11,3 +11,19 @@ DOM - PREPA 8 : « Couleurs » :
    (utilisez soit le nom anglais de la couleur, soit le format hexadécimal, soit le format RGB) :' ;
 2. Utilisez ces couleurs pour satisfaire les préférences de votre utilisateur
 */
+
+let sColor = prompt("Choisissez une couleur à appliquer aux titres :");
+let sBackground = prompt("Choisissez une couleur de fond à appliquer aux titres:");
+
+console.log(sColor);
+console.log(sBackground);
+
+let h1Elt = document.getElementsByTagName("h1");
+
+for (let i = 0; i < h1Elt.length; i++) {
+    h1Elt[i].style.color = sColor;
+    h1Elt[i].style.backgroundColor = sBackground;
+}
+
+
+
